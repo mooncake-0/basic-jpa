7d6054d3b626c6a8f5b69c2e711f6ed36a5a5b92
wrock.kang <a@moiming.net>
2022-12-21 00:31:20 +0900

    221221 Commit

--- src/test/java/com/study/jpa/understanding/JpaTest.java
+++ src/test/java/com/study/jpa/understanding/JpaTest.java
@@ -18,7 +18,6 @@ public class JpaTest {
         emf = Persistence.createEntityManagerFactory("helloJpa");
         entityManager = emf.createEntityManager();
         tx = entityManager.getTransaction();
-
     }
 
     @Test
@@ -80,7 +79,7 @@ public class JpaTest {
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
-        }finally {
+        } finally {
             entityManager.close();
         }
         emf.close();
@@ -101,9 +100,90 @@ public class JpaTest {
             tx.commit(); // 커밋할때는 1차 캐시 기반으로 변경을 감지함. (update 쿼리가 알아서 나감)
         } catch (Exception e) {
             tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("Flush Test :: 쿼리 시점 확인")
+    void test4() {
+
+        tx.begin();
+
+        try {
+
+            Member memberA = new Member(100L, "MemberA");
+            entityManager.persist(memberA);
+
+            entityManager.flush();
+
+            System.out.println("====================이 라인 이전에 FLUSH 실행 (SQL 나갈 것으로 예상)");
+
+            tx.commit();
+
+            System.out.println("====================트랜잭션 커밋 호출");
+
+        } catch (Exception e) {
+            tx.rollback();
+
+        } finally {
+            entityManager.close();
+        }
+        emf.close();
+    }
+
+    @Test
+    @DisplayName("준영속 상태 :: detach 후에는 쿼리가 나가지 않는다")
+    void test5() {
+
+        tx.begin();
+
+        try {
+
+            Member findMember = entityManager.find(Member.class, 100L);
+            findMember.setName("Change Name");
+            System.out.println("================ 변경사항이 발생: member.Name to [Change Name]");
+
+            entityManager.detach(findMember);
+            System.out.println("================ Member Detach 발생: 영컨이 더이상 관리하지 않고, 1차 캐시에서 분리");
+
+            tx.commit();
+            System.out.println("================ Commit 호출: 1차 캐시에 없기 때문에 더티캐싱이 안됨. Update 쿼리 생성 안됨");
+
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+        emf.close();
+    }
+
+
+    @Test
+    @DisplayName("준영속 상태 : EntityManager.clear()시 영컨과 1차 캐시를 비운다")
+    void test6() {
+        tx.begin();
+
+        try {
+
+            Member findA = entityManager.find(Member.class, 100L);
+
+            entityManager.clear();
+            System.out.println("=================== EM.clear() 발생: 영컨 및 1차 캐시가 비워진다");
+
+            Member findB = entityManager.find(Member.class, 100L);
+            System.out.println("=================== 다시 같은 객체 조회 발생: 1차 캐시에 100L의 Member가 없기 때문에 Select 쿼리 다시 발생");
+
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
         }finally {
             entityManager.close();
         }
+
         emf.close();
     }
 }
